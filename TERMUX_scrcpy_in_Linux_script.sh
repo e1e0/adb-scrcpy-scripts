fd9ce8c40acfe3cec24fb8952d351bf9f23cd6a2
[pause, if needed] <91560320+e1e0@users.noreply.github.com>
2022-06-21 18:57:19 +0100

    Update TERMUX_scrcpy_in_Linux_script.sh

--- TERMUX_scrcpy_in_Linux_script.sh
+++ TERMUX_scrcpy_in_Linux_script.sh
@@ -1,2 +1,8 @@
 #! /data/data/com.termux/files/usr/bin/bash
-su -c "/data/data/ru.meefik.linuxdeploy/files/bin/linuxdeploy -p linux start -m"
+
+command='start -m'
+if [[ $1 == 'STOP' ]]; then
+  command='stop -u'
+fi
+
+su -c "/data/data/ru.meefik.linuxdeploy/files/bin/linuxdeploy -p linux $command"
