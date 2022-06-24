19464a7d6eb1c5400ee81be031094b69d3e2f993
[pause, if needed] <91560320+e1e0@users.noreply.github.com>
2022-06-24 05:43:47 +0100

    Update LINUX_CONTAINER_execute_adb_scrcpy.sh

--- LINUX_CONTAINER_execute_adb_scrcpy.sh
+++ LINUX_CONTAINER_execute_adb_scrcpy.sh
@@ -8,16 +8,19 @@ adb-scrcpy-scripts: Bash scripts for Android screen\n\
 
 read -e -i "192.168.[num.num]" -p "device IP address: "
 
-echo "Connecting to \"$REPLY\"..."
-adb connect $REPLY
+i=1
+while ($i < 2); do
+  echo "Connecting to \"$REPLY\"..."
+  adb connect $REPLY
 
-if [[ $(adb devices | grep 'unauthorized') == "" ]]; then
-  declare -x SDL_VIDEODRIVER='x11'
-  scrcpy --render-driver=software
-else
-  echo "Can't connect! :-("
-  adb reconnect offline
-  sleep 12s
-  declare -x SDL_VIDEODRIVER='x11'
-  scrcpy --render-driver=software
-fi
+  if [[ $(adb devices | grep 'unauthorized') == "" ]]; then
+    declare -x SDL_VIDEODRIVER='x11'
+    scrcpy --render-driver=software
+    break
+  else
+    echo "Can't connect! :-("
+    adb reconnect offline
+    sleep 10s
+    i += 1
+  fi
+done
